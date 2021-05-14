9941757ae11592923cac152f47214917b03793c5
Oliver Steele <steele@osteele.com>
2021-05-14 23:15:29 +0800

    Move more functions to utils.js

--- sketch.js
+++ sketch.js
@@ -193,29 +193,3 @@ function mouseMoved() {
 const findProjectUnderMouse = () => projects.find(projectIsUnderMouse);
 const projectIsUnderMouse = ({ x, y }) =>
   x <= mouseX && mouseX < x + colWidth && y <= mouseY && mouseY < y + rowHeight;
-
-const lowerSnakeCase = (s) => s.replace(/ /g, "_").toLowerCase();
-
-const renameKeys = (obj, keyTransformer) =>
-  Object.fromEntries(
-    Object.entries(obj).map(([k, v]) => [keyTransformer(k), v])
-  );
-
-const withoutOxford = (ar, s) =>
-  ar.length <= 2
-    ? ar.join(s)
-    : ar.slice(0, ar.length - 1).join(", ") + s + ar[ar.length - 1];
-
-function periodicEase(x, period) {
-  let q = floor(x / period) * period;
-  return easeInOutQuad(x % period, q, period, period);
-}
-
-// const periodicEase = (x, period) => floor(x / period) * period + ease(x % period);
-
-// const ease = x => easeInOut
-
-function easeInOutQuad(t, b, c, d) {
-  if ((t /= d / 2) < 1) return c / 2 * t * t + b;
-  return -c / 2 * ((--t) * (t - 2) - 1) + b;
-}
--- utils.js
+++ utils.js
@@ -11,3 +11,25 @@ function rotateAbout(angle, x, y) {
     rotate(angle);
     translate(-x, -y);
 }
+
+const lowerSnakeCase = (s) => s.replace(/ /g, "_").toLowerCase();
+
+const renameKeys = (obj, keyTransformer) =>
+    Object.fromEntries(
+        Object.entries(obj).map(([k, v]) => [keyTransformer(k), v])
+    );
+
+const withoutOxford = (ar, s) =>
+    ar.length <= 2
+        ? ar.join(s)
+        : ar.slice(0, ar.length - 1).join(", ") + s + ar[ar.length - 1];
+
+function periodicEase(x, period) {
+    let q = floor(x / period) * period;
+    return easeInOutQuad(x % period, q, period, period);
+}
+
+function easeInOutQuad(t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+    return -c / 2 * ((--t) * (t - 2) - 1) + b;
+}
