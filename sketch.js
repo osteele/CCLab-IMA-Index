c4ada0350415a4fe3ebeb5aa789f471cbb69d4ce
Oliver Steele <steele@osteele.com>
2021-05-14 15:35:43 +0800

    show title in header scroll

--- sketch.js
+++ sketch.js
@@ -36,14 +36,14 @@ function draw() {
 
   const selectedProject = findProjectUnderMouse();
   if (selectedProject) {
-    drawHeaderProject(selectedProject, true);
+    drawHeaderProject(selectedProject, { includeInstructions: true });
   } else {
     push();
     const tx = -frameCount;
-    const pw = width * 2 / 3;
-    translate(tx % (3 * pw), 0);
-    for (let i = 0; i < 10; i++) {
-      drawHeaderProject(projects[i]);
+    const pw = width * 2 / 3 + 20;
+    translate(tx % (pw * projects.length), 0);
+    for (let i = 0; i < projects.length + 2; i++) {
+      drawHeaderProject(projects[i % projects.length], { includeTitle: true });
       translate(pw, 0);
     }
     pop();
@@ -56,23 +56,28 @@ function draw() {
   projects.forEach((p) => drawProject(p, projectIsUnderMouse(p)));
 }
 
-function drawHeaderProject({ image: img, description, instructions }, includeInstructions) {
-  const s = min(window.width / 3 / img.width, 250 / img.height);
+function drawHeaderProject({ name, image: img, description, instructions }, { includeTitle, includeInstructions }) {
+  const topPadding = includeTitle ? 20 : 10;
+  const s = min(window.width / 3 / img.width, (headerHeight - topPadding) / img.height);
   const w = img.width * s;
   const c1 = w + 10;
   const c2 = (2 * windowWidth) / 3;
 
-  image(img, 0, 0, w, img.height * s);
+  image(img, 0, topPadding, w, img.height * s);
 
-  textAlign(LEFT);
   fill("black");
+  textAlign(LEFT);
   textSize(12);
 
-  textStyle(BOLD);
-  // text("Description", c1, 20, windowWidth / 3 - 10);
-  includeInstructions && instructions && text("Instructions", c2, 20, windowWidth / 3 - 10);
+  textStyle(BOLDITALIC);
+  includeTitle && text(name, 0, 15);
+
   textStyle(ITALIC);
-  text(description, c1, 15, c2 - c1 - 10);
+  text(description, c1, topPadding, c2 - c1 - 10);
+
+  textStyle(BOLD);
+  includeInstructions && instructions && text("Instructions", c2, topPadding, windowWidth / 3 - 10);
+
   textStyle(NORMAL);
   includeInstructions && text(instructions, c2, 40, windowWidth / 3 - 10);
 }
@@ -111,10 +116,10 @@ function drawProject({ name, authors, x, y }, highlight) {
     noStroke();
     rect(0, 0, colWidth, rowHeight, 10);
 
-    fill("red");
-    textSize(18);
-    text("Click to play", 0, 65, colWidth - 20);
   }
+  fill(highlight ? "red" : 235);
+  textSize(18);
+  text("Click to play", 0, 60, colWidth - 20);
 
   fill("black");
   textFont("Roboto");
